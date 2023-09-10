7e357132f74a2b521a86b359c54bbef12a747d65
gasbodd13 <gasbodd13@newhaven.edu>
2023-09-10 00:31:06 -0400

    missed adding foldername in TestingSingle file

--- preProcessor/TestingSingle.py
+++ preProcessor/TestingSingle.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import *
 from openpyxl import load_workbook
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from issmfilelog import logger
+from preProcessor.issmfilelog import logger
 from selenium.webdriver.support import expected_conditions as ec
 
 class ProgressBar:
