f93f89cf21b2934ecc61bb238d43cf6edcdfbe5b
gashirdi <gaskumm3@newhaven.edu>
2023-09-19 11:38:36 -0400

    updated Title

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
     <MyContextProvider>
       <html lang="en">
         <body className={font.className}>
-          {/* <body> */}
           {children}
           <Footer />
           <Toaster />
