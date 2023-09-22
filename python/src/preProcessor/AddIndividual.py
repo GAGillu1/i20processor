3eb4363bc557626b0c46f33cc8a693f182311b8d
gasbodd13 <gasbodd13@newhaven.edu>
2023-09-22 10:05:35 -0400

    loggers additions

--- python/src/preProcessor/AddIndividual.py
+++ python/src/preProcessor/AddIndividual.py
@@ -5,18 +5,19 @@ import configparser
 from selenium.common.exceptions import NoSuchElementException
 from preProcessor.issmfilelog import logger
 from selenium.webdriver.support import expected_conditions as ec
-
+import inspect
 def AddIndividual(student, driver, check_val):
     config = configparser.ConfigParser()
     config.read('preProcessor/config.ini')
     try:
-        # print("start of AddIndividual")
+        logger.info("start of AddIndividual")
         wait = WebDriverWait(driver, 10)  # 10 seconds timeout
         #  print(student)
         #  template_url = domain_url + '/WizardPickTemplate.aspx?SysId=56527&Mode=F'
         # driver.get(domain_url + '/AddNewIndividual.aspx')
         # Find the username and password input fields using XPath
         if check_val:
+            logger.info("Inside check val true condition")
             element = wait.until(
                 ec.visibility_of_element_located(
                     (By.XPATH, config['ID_XPATH']['last_name'])))
@@ -34,18 +35,20 @@ def AddIndividual(student, driver, check_val):
             Select(driver.find_element(By.ID, config['ID_XPATH']['department_id'])).select_by_visible_text(student.Department)
             # print("department id filled")
             driver.find_element(By.ID, config['ID_XPATH']['continue_btn']).click()
-
+            logger.info("after continue button add individual.aspx")
         try:
             time.sleep(2)
             ignore_and_add_element = wait.until(
                 ec.element_to_be_clickable((By.ID, config['ID_XPATH']['ignore_and_add'])))
             ignore_and_add_element.click()
+            logger.info("after ignore and add")
             # driver.find_element(By.ID, config['ID_XPATH']['ignore_and_add']).click()
             # check_val = True
             # print(f"duplicate caught duplicate_check function for {student.CampusID}")
             # raise Exception
             # # Perform actions with the error element (if needed)
         except NoSuchElementException:
+            logger.info("no such element exception")
             # print(f"no exceptions caught duplicate_check function for {student.CampusID}")
             # check_val = False
             pass
@@ -53,6 +56,7 @@ def AddIndividual(student, driver, check_val):
         #  ----------------Choose Template ---------------------------#
         #  need to modify the below code according to config.ini parameters
         time.sleep(2)
+        logger.info("before select template")
         # Use the WebDriverWait to wait for the element to be visible and enabled
         element = wait.until(ec.element_to_be_clickable((By.ID, config['ID_XPATH']['select_template'])))
 
@@ -60,62 +64,64 @@ def AddIndividual(student, driver, check_val):
         Select(element).select_by_visible_text(student.Template)
         # Select(driver.find_element(By.ID, config['ID_XPATH']['select_template'])).select_by_visible_text(student.Template)
         # print("template filled")
+        logger.info("after selecting template")
         driver.find_element(By.ID, config['ID_XPATH']['continue_btn']).click()
         # ----------------Fill F/M Information---------------------------#
         #  https://issm-test.newhaven.edu/WizardPickTemplate.aspx?SysId=13019&Mode=F
+        logger.info("after continue button template")
         time.sleep(2)
         # Wait for and select an option in 'country_of_birth' element
         country_of_birth_element = wait.until(
             ec.presence_of_element_located((By.ID, config['ID_XPATH']['country_of_birth'])))
         Select(country_of_birth_element).select_by_visible_text(student.BirthCountry)
-
+        logger.info("after birth county")
         # Select(driver.find_element(By.ID, config['ID_XPATH']['country_of_birth'])).select_by_visible_text(student.BirthCountry)
         # print("birth country filled")
         Select(driver.find_element(By.ID, config['ID_XPATH']['country_of_citizenship'])).select_by_visible_text(student.Citizenship)
-        # print("citizenship filled")
+        logger.info("citizenship filled")
         #  P4LevelEduDDL
         Select(driver.find_element(By.ID, config['ID_XPATH']['level_of_edu'])).select_by_visible_text(student.Level)
-        # print("level of education filled")
+        # logger.info("level of education filled")
         #  major =
         Select(driver.find_element(By.ID, config['ID_XPATH']['major_of_study'])).select_by_visible_text(student.Major)
-        # print("major filled")
+        # logger.info("major filled")
         #  startDate
         driver.find_element(By.XPATH, config['ID_XPATH']['course_start_date']).send_keys(student.StartDate)
-        # print("course start date filled")
+        # logger.info("course start date filled")
         #  sessionStartDate
         driver.find_element(By.XPATH, config['ID_XPATH']['initiate_start_date']).send_keys(student.SessionStartDate)
-        # print("initial state date filled")
+        # logger.info("initial state date filled")
         #  endDate
         driver.find_element(By.XPATH, config['ID_XPATH']['course_end_date']).send_keys(student.EndDate)
-        # print("end date filled")
+        # logger.info("end date filled")
         #  academicTerm
         Select(driver.find_element(By.ID, config['ID_XPATH']['academic_term'])).select_by_visible_text(str(student.AcademicTerm))
         # logger.info("academic term filled")
         #  tuitionAndFees
         driver.find_element(By.XPATH, config['ID_XPATH']['tuition_expenses']).send_keys(student.Tuition)
-        # print("tuition filled")
+        # logger.info("tuition filled")
         #  livingExpenses
         driver.find_element(By.XPATH, config['ID_XPATH']['living_expenses']).send_keys(student.LivingExpenses)
-        # print("living expenses filled")
+        # logger.info("living expenses filled")
         #  otherDesc
         driver.find_element(By.XPATH, config['ID_XPATH']['oth_expense_type']).send_keys(student.OtherDesc)
-        # print("oth expenses type filled")
+        # logger.info("oth expenses type filled")
         #  otherValue
         driver.find_element(By.XPATH, config['ID_XPATH']['other_expense']).send_keys(student.OtherAmount)
-        # print("other expense filled")
+        # logger.info("other expense filled")
         driver.find_element(By.XPATH, config['ID_XPATH']['personal_funds']).send_keys(student.PersonalFunds)
-        # print("personal funds filled")
+        # logger.info("personal funds filled")
         driver.find_element(By.XPATH, config['ID_XPATH']['school_funds']).send_keys(student.SchoolFunds)
-        # print("school funds filled")
+        # logger.info("school funds filled")
         driver.find_element(By.XPATH, config['ID_XPATH']['school_funds_desc']).send_keys(student.SchoolFundsDesc)
-        # print("school funds desc filled")
+        # logger.info("school funds desc filled")
         driver.find_element(By.XPATH, config['ID_XPATH']['other_funds']).send_keys(student.FundsFromAnother)
-        # print("other funds filled")
+        # logger.info("other funds filled")
         driver.find_element(By.XPATH, config['ID_XPATH']['other_funds_desc']).send_keys(student.FundsFromAnotherDesc)
-        # print("other funds desc filled")
+        # logger.info("other funds desc filled")
         driver.find_element(By.XPATH, config['ID_XPATH']['remarks']).send_keys(student.Remarks)
         #  Click Continue
-        # print("remarks filled")
+        # logger.info("remarks filled")
         driver.find_element(By.ID, config['ID_XPATH']['continue_btn']).click()
         #  can reference same continue button as id is same
         # ----------------Fill Additional Information---------------------------#
@@ -126,26 +132,26 @@ def AddIndividual(student, driver, check_val):
         Select(gender_element).select_by_visible_text(student.Gender)
         # Select(driver.find_element(By.ID, config['ID_XPATH']['gender'])).select_by_visible_text(student.Gender)
         #  Click Add Permanent Address
-        # print("gender filled")
+        logger.info("gender filled")
         driver.find_element(By.ID, config['ID_XPATH']['add_permanent_addr']).click()
         # ----------------Fill Add/Edit Address Information---------------------------#
-        # print("add_permanent_addr")
+        logger.info("add_permanent_addr")
         element = wait.until(
             ec.visibility_of_element_located(
                 (By.XPATH, config['ID_XPATH']['address_one'])))
         element.send_keys(student.PermLine1)
         # driver.find_element(By.XPATH, config['ID_XPATH']['address_one']).send_keys(student.PermLine1)
-        # print("address_one")
+        # logger.info("address_one")
         driver.find_element(By.XPATH, config['ID_XPATH']['address_two']).send_keys(student.PermLine2)
-        # print("address_two")
+        # logger.info("address_two")
         driver.find_element(By.XPATH, config['ID_XPATH']['address_city']).send_keys(student.PermCity)
-        # print("address_city")
+        # logger.info("address_city")
         driver.find_element(By.XPATH, config['ID_XPATH']['address_province']).send_keys(student.PermProvince)
-        # print("address_province")
+        # logger.info("address_province")
         driver.find_element(By.XPATH, config['ID_XPATH']['address_zip_code']).send_keys(student.PermPostal)
-        # print("address_zip_code")
+        # logger.info("address_zip_code")
         Select(driver.find_element(By.ID, config['ID_XPATH']['address_country'])).select_by_visible_text(student.PermCountry)
-        # print("address_country")
+        # logger.info("address_country")
         driver.find_element(By.XPATH, config['ID_XPATH']['phone_number']).send_keys(student.Phone)
         #  Click Save Permanent Address
         driver.find_element(By.ID, config['ID_XPATH']['submit_btn']).click()
@@ -190,5 +196,7 @@ def AddIndividual(student, driver, check_val):
             logger.error(f"inside final status : {final_status.text}")
             return False
     except Exception as e:
+        line_number = inspect.currentframe().f_lineno
+        logger.error(f"Exception raised in AddIndividual function (line {line_number}): {e}")
         logger.error(f"failed inside AddIndividual function of AddIndividual.py {e}")
         return False
