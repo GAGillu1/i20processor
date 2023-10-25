db49a0e5b5bd6b6b8f0c1eb94193477cf9bb1c91
gasbodd13 <gasbodd13@newhaven.edu>
2023-10-25 14:57:11 -0400

    pusing code by removing headless

--- python_code/src/preProcessor/chrome_browser_test.py
+++ python_code/src/preProcessor/chrome_browser_test.py
@@ -1,16 +1,16 @@
 from selenium import webdriver
 
 try:
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    # chrome_options.binary_location = '/usr/bin/google-chrome'
-    chrome_options.page_load_strategy = 'normal'
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # # chrome_options.binary_location = '/usr/bin/google-chrome'
+    # chrome_options.page_load_strategy = 'normal'
     print("before start of webdriver")
     # browser = webdriver.Chrome(service=service, options=chrome_options)
     # commented the below line for amazon instance.
-    browser = webdriver.Chrome(options=chrome_options)
+    browser = webdriver.Chrome()
     print("before get")
     browser.get("https://www.google.com")
     print("after get")
