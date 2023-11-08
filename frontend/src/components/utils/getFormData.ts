66f4f969236c0b09ffe49bae955f67f72fc8c54a
gashirdi <gaskumm3@newhaven.edu>
2023-11-08 13:27:10 -0500

    fixed encryption in pre-processor

--- frontend/src/components/utils/getFormData.ts
+++ frontend/src/components/utils/getFormData.ts
@@ -1,5 +1,4 @@
 import forge from "node-forge";
-import { JSEncrypt } from "jsencrypt";
 
 export default function getFormData(val: Object) {
   let fD = new FormData();
@@ -9,33 +8,6 @@ export default function getFormData(val: Object) {
   return fD;
 }
 
-// function encrypt(message: string) {
-//   const publicKeyPem = `-----BEGIN PUBLIC KEY-----
-// MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDEIqKz8UVPBuFKwOKgK4zubIXf
-// nBR2XJLgkyS1ruQGmC2OVfzoDuL93H+yf70Q0tPZIr8+/VB5OjE7TjhtB7uHi6hp
-// 9mDNH7l2eAT8EVeueSQ+s8SiJ+B2GbE0xQtKNWnPtnVgY2xkNtNKYqRwMbLbdHFO
-// zUdPwNtnljnCoAXiZQIDAQAB
-// -----END PUBLIC KEY-----`;
-
-//   const publicKey = forge.pki.publicKeyFromPem(publicKeyPem);
-
-//   console.log("Public Key", publicKey);
-
-//   const encryptedMessage = publicKey.encrypt(
-//     forge.util.encodeUtf8(message),
-//     "RSA-OAEP",
-//     {
-//       md: forge.md.sha1.create(),
-//     }
-//   );
-
-//   console.log("Encrypted: ", encryptedMessage);
-
-//   const encryptedMessageBase64 = forge.util.encode64(encryptedMessage);
-//   console.log("Encoded: ", encryptedMessageBase64);
-//   return encryptedMessageBase64;
-// }
-
 function encrypt(message: string) {
   const publicKey = forge.pki.publicKeyFromPem(`-----BEGIN PUBLIC KEY-----
   MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDEIqKz8UVPBuFKwOKgK4zubIXf
@@ -43,7 +15,6 @@ function encrypt(message: string) {
   9mDNH7l2eAT8EVeueSQ+s8SiJ+B2GbE0xQtKNWnPtnVgY2xkNtNKYqRwMbLbdHFO
   zUdPwNtnljnCoAXiZQIDAQAB
   -----END PUBLIC KEY-----`);
-  console.log("Public Key", publicKey);
 
   const encryptedMessage = publicKey.encrypt(
     forge.util.encodeUtf8(message),
@@ -55,10 +26,7 @@ function encrypt(message: string) {
       },
     }
   );
-
-  console.log("Encrypted: ", encryptedMessage);
-  const encryptedMessageBase64 = forge.util.encode64(encryptedMessage);
-  console.log("Encoded: ", encryptedMessageBase64);
+  return forge.util.encode64(encryptedMessage);
 }
 
 export function getEncryptedFormData(val: Object) {
