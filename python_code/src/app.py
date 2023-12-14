2e6bf63a1673e5aadeab2d024dc7afcbb7cb88a9
I Govardhan <126288447+GAGillu1@users.noreply.github.com>
2023-12-14 10:45:38 -0500

    Updated to handle empty dataframes

--- python_code/src/app.py
+++ python_code/src/app.py
@@ -888,8 +888,10 @@ def isntance():
     if request.method == 'GET':
         institutionid = request.headers.get('institutionid')
         result = instanceget(institutionid)
-        print(";;;;;",result)
-        result_dict = result.to_dict(orient='records')  
+        if isinstance(result,str):
+            return jsonify({'message':'No Instances','data':[]})
+
+        result_dict = result.to_dict(orient='records')
         return jsonify({'message': 'Fetched instances', 'data': result_dict})
 @app.route('/instance/test',methods=['GET','POST'])
 def instancetesting():
